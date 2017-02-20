eb64784cc10e52f532df869c20727193d0b4d746
dilipdhanush <dilipcse97@gmail.com>
2017-02-20 21:34:50 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,1 +1,11 @@
 console.log('Loaded!');
+    var img=document.getElementById('anony');
+    var marginLeft=0;
+    img.onclick(){
+        var interval=setInterval(moveRight,20);
+    }
+    function moveRight(){
+        marginLeft=marginLeft+1;
+        img.style.marginLeft=marginleft+"px";
+    }
+    
